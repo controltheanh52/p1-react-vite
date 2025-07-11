1b6bb8d7dadfc7aff8e9c31f7bd52282abe1e5a1
NguyenTheAnh0502 <s3975844@rmit.edu.vn>
2025-07-11 22:41:05 +0700

    Video 31 11:18

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,10 +3,34 @@
 
 import './style.css';
 const MyComponent = () => {
+    const theanh = "Control";
+    const num = 21;
+    //const bool = true;//boolean, undifined, and null datatype do not print out
+    //const unde = undifined;
+    //const null = null; 
+    const arr = [1, 2, 3];
+
+    const obj = {
+        name: "Nguyen The Anh",
+        age: 21
+    }
   return (
-    <>//fragment
-        <div>Control & The Anh update</div>
-        <div className="child">this is a child</div>
+    //fragment
+    //JSON.stringify: convert the arr or obj to be a json datatype
+    <>
+        <div> {theanh} - Control & The Anh update</div>
+        
+        <div className="child" 
+            style= {
+                {borderRadius: "10px"}
+            }
+        >{num} - this is a child</div>
+
+        <div>{arr.join(", ")}</div>
+        
+        <div>{JSON.stringify(obj)}</div>
+
+        <div>{console.log("Coding on the html with curly bracket")}</div>
     </>
   );
 }
